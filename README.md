22eb24a27c496f01b77e5ba1837d499d36293201
Michael Stahnke <stahnke@floxdev.com>
2024-01-25 22:28:25 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,39 @@
+# hubot-cantchart
+
+Are you tired of your management team and co-workers having expectations about your work? We've experienced dozens or hundreds of reasons over the years that work can't be done -- even when critical. To show how you can't get any work done, you just put yourself on the can't chart. 
+
+This is extension pulls for the wisdom of the crowd on [websages/hates-software/issues/1](https://github.com/websages/hates-software/issues/1), which is pertty much just perfect.
+
+
+![Gantt Chart](https://i.imgur.com/HNBkC1B.png)
+
+![Cant Chart](https://i.imgur.com/k5HfIYT.png)
+
+
+# Installation
+
+`npm i hubot-cantchart --save`
+
+add `hubot-cantchart` to your `external-scripts` file.
+
+# Configuration
+
+You need a `GITHUB_TOKEN` environment variable set so you can make API calls to github.
+
+# Usage
+
+    hubot excuse - get a new excuse for why you can't get any work done
+
+    hubot cant chart -- alias for excuse
+
+    hubot can't chart -- alias for excuse
+
+
+# Attribution
+
+The concept of a "cant chart" came from [rick](https://github.com/rick). (Mr Cant Chart himself). See [the full presentation](https://www.slideshare.net/ogc/waxing-ballroom-floors-on-the-titani) for all the fixin's. (slide 58+)
+
+See also: [websages/hates-software](https://github.com/websages/hates-software/issues/)
+
+# License 
+MIT
---
+++ src/cantchart.js
@@ -0,0 +1,60 @@
+// Description:
+//   Get a new excuse for why you can't get any work done
+//
+// Dependencies:
+//   axios
+//
+// Configuration:
+//  GITHUB_TOKEN
+//
+// Commands:
+//   hubot can't chart -- get a new excuse for why you can't get any work done
+//   hubot exuse -- put yourself on the can't chart 
+//
+// Author: stahnma, websages
+//
+
+// This is a script for Hubot that will get a random comment from a GitHub
+// issue, in this case hard-coded to be issue#1 at websages/hates-sofware
+module.exports = function(robot) {
+
+  const axios = require('axios');
+
+  const username = 'websages';
+  const repository = 'hates-software';
+  const issueNumber = 1;
+
+  const baseURL = 'https://api.github.com/repos/';
+  const authHeader = 'Bearer';
+  const token = process.env.GITHUB_TOKEN || 'YOUR_ACCESS_TOKEN';
+  if (token == 'YOUR_ACCESS_TOKEN' ) {
+    robot.logger.error('GITHUB_TOKEN not set');
+    return;
+  }
+  const url = `${baseURL}${username}/${repository}/issues/${issueNumber}/comments`;
+
+  robot.respond(/\S*excuse\S*|\S*cant chart\S*|can\'t chart\S*/i, function(msg) {
+    axios.get(url, {
+        headers: {
+          Authorization: `${authHeader} ${token}`,
+        },
+      })
+      .then(response => {
+        const comments = response.data;
+        if(comments.length === 0) {
+          console.log('No comments found for the specified issue.');
+          process.exit(1);
+        }
+
+        const randomComment = comments[Math.floor(Math.random() * comments.length)];
+        robot.logger.debug(`Author: ${randomComment.user.login}`);
+        robot.logger.debug(`Comment Body: ${randomComment.body}`);
+        msg.send('"' + randomComment.body + '" -- ' + randomComment.user.login);
+
+      })
+      .catch(error => {
+        console.error('Error making request:', error.message);
+        process.exit(1);
+      });
+  });
+}
